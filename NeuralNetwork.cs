7cb686fb97e55521218c644a7822da6f44b2a2d8
xxfast <isuru@xfastgames.com>
2017-05-13 04:37:34 +1000

    Added the initial commit
    Includes a static neural network implementation written in C# along
    with a utility function to standardise inputs

---
+++ NeuralNetwork.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.Distributions;
+
+namespace xann
+{
+	public class NeuralNetwork
+	{
+		private int _nI; // number of inputs
+		private int _nH; // number of hidden 
+		private int _nO; // number of outputs
+
+		private Matrix<float> _iW; // input weights
+		private Matrix<float> _oW; // output weights
+
+		private Matrix<float> _aH; // activity of hidden 
+		private Matrix<float> _aO; // activity of Output 
+
+		private Matrix<float> _AH; // activation of hidden 
+		private Matrix<float> _AO; // activation of hidden 
+
+		public NeuralNetwork(int inputs, int hidden, int outputs)
+		{
+			_nI = inputs;
+			_nH = hidden;
+			_nO = outputs;
+		}
+
+		public NeuralNetwork() : this(2, 3, 1) 
+		{
+			_iW = Matrix<float>.Build.Random(_nI, _nH);
+			_oW = Matrix<float>.Build.Random(_nH, _nO);
+		}
+
+		public Matrix<float> Forward(Matrix<float> input) {
+			_aH = input.Multiply(_iW);
+			_AH = Sigmoid(_aH);
+			_aO = _AH.Multiply(_oW);
+			_AO = Sigmoid(_aO);
+			return _AO;
+		}
+
+		public Matrix<float> Sigmoid(Matrix<float> z)
+		{
+			float[] outs = new float[z.RowCount * z.ColumnCount];
+			int i=0;
+			foreach (float x in z.Enumerate())
+			{
+				outs[i++] = 1 / (1 + (float)Math.Exp(-x));
+			}
+			return Matrix<float>.Build.Dense(z.RowCount,z.ColumnCount,outs);
+		}
+	}
+}
---
+++ Utilities.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using MathNet.Numerics.LinearAlgebra;
+
+namespace xann
+{
+	public static class Utilities
+	{
+		public static Matrix<float> Standerdise(Matrix<float> to)
+		{
+			float[] outs = new float[to.RowCount * to.ColumnCount];
+			int n = 0;
+			float[] maxs = new float[to.ColumnCount];
+			int i=0;
+			foreach (Vector<float> cols in to.EnumerateColumns())
+			{
+				maxs[i] = cols[0];
+				foreach (float val in cols) 
+					maxs[i] = Math.Max(maxs[i], val);
+				foreach (float val in cols) 
+					outs[n++] = val/maxs[i];
+				i++;
+			}
+			return Matrix<float>.Build.Dense(to.RowCount, to.ColumnCount, outs);
+		}
+	}
+}
